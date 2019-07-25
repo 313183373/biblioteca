314ac548fe5ba7b4f2cdfe7efbdab974a1e9b669
xion <313183373@qq.com>
2019-07-25 22:48:27 +0800

    1.7: add checkout a book test

--- test/com/twu/biblioteca/CheckoutBookTest.java
+++ test/com/twu/biblioteca/CheckoutBookTest.java
@@ -1,11 +1,21 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.TestDouble.DummyOutputStream;
+import com.twu.biblioteca.TestDouble.PrintStreamSpy;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CheckoutBookTest {
     @Test
     public void ShouldCheckoutABookSuccessfully() {
+        PrintStreamSpy out = new PrintStreamSpy(new DummyOutputStream());
+        BibliotecaView bibliotecaView = new BibliotecaView(out);
+        BibliotecaBorrow bibliotecaBorrow = new BibliotecaBorrow(bibliotecaView);
 
-        BibliotecaBorrow bibliotecaBorrow = new BibliotecaBorrow()
+        String bookTitle = BibliotecaBooks.books.get(0).getTitle();
+        bibliotecaBorrow.borrowABook(bookTitle);
+        assertThat(out.getContent(), is("Thank you! Enjoy the book"));
     }
 }
