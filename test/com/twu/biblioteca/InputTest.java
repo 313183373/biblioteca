228348d42b9dcaca5743a20fc0c01536755c1add
xion <313183373@qq.com>
2019-07-25 17:11:32 +0800

    1.5: add test for get notified when chose an invalid option

---
+++ test/com/twu/biblioteca/InputTest.java
@@ -0,0 +1,26 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.TestDouble.DummyOutputStream;
+import com.twu.biblioteca.TestDouble.PrintStreamSpy;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static com.twu.biblioteca.BibliotecaInput.INVALID_OPTION;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class InputTest {
+
+    @Test
+    public void ShouldGetNotifiedWhenChooseAnInvalidOption() {
+        PrintStreamSpy out = new PrintStreamSpy(new DummyOutputStream());
+        BibliotecaView bibliotecaView = new BibliotecaView(out);
+        BibliotecaInput bibliotecaInput = new BibliotecaInput(bibliotecaView, new Scanner("2"));
+
+        int selection = bibliotecaInput.getUserSelection();
+
+        assertThat(out.getContent(), is("Please select a valid option!"));
+        assertThat(selection, is(INVALID_OPTION));
+    }
+}
