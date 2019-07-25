b4ecbf9264495e673572a9f06b526ce3cf12b735
xion <313183373@qq.com>
2019-07-25 15:57:41 +0800

    1.4: view a menu of options test

---
+++ test/com/twu/biblioteca/MenuTest.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.TestDouble.DummyOutputStream;
+import com.twu.biblioteca.TestDouble.PrintStreamSpy;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MenuTest {
+
+    @Test
+    public void ShouldPrintMenuListOfOptions() {
+        PrintStreamSpy out = new PrintStreamSpy(new DummyOutputStream());
+        BibliotecaView bibliotecaView = new BibliotecaView(out);
+        bibliotecaView.showMenu();
+        assertThat(out.getContent(), is("1. List of books"));
+    }
+}
