c61911c2bb19a0d6edb0fa4cd10490df55fd1832
xion <313183373@qq.com>
2019-07-25 20:37:22 +0800

    1.3: add book data

--- src/com/twu/biblioteca/BibliotecaBooks.java
+++ src/com/twu/biblioteca/BibliotecaBooks.java
@@ -7,16 +7,16 @@ public class BibliotecaBooks {
     private ArrayList<BibliotecaBook> books = new ArrayList<>(Arrays.asList(BOOKS));
 
     public final static BibliotecaBook[] BOOKS = {
-            new BibliotecaBook("In Search of Lost Time"),
-            new BibliotecaBook("Don Quixote"),
-            new BibliotecaBook("Ulysses"),
-            new BibliotecaBook("The Great Gatsby"),
-            new BibliotecaBook("Moby Dick"),
-            new BibliotecaBook("Hamlet"),
-            new BibliotecaBook("War and Peace"),
-            new BibliotecaBook("The Odyssey"),
-            new BibliotecaBook("One Hundred Years of Solitude"),
-            new BibliotecaBook("The Divine Comedy"),
+            new BibliotecaBook("In Search of Lost Time", "Marcel Proust", "1998"),
+            new BibliotecaBook("Don Quixote", "Miguel de Cervantes", "1999"),
+            new BibliotecaBook("Ulysses", "James Joyce", "1997"),
+            new BibliotecaBook("The Great Gatsby", "F.Scott Fitzgerald", "1987"),
+            new BibliotecaBook("Moby Dick", "Herman Melville", "1980"),
+            new BibliotecaBook("Hamlet", "William Shakespeare", "2000"),
+            new BibliotecaBook("War and Peace", "Leo Tolstoy", "2001"),
+            new BibliotecaBook("The Odyssey", "Homer", "1993"),
+            new BibliotecaBook("One Hundred Years of Solitude", "Gabriel Garcia Marquez", "2000"),
+            new BibliotecaBook("The Divine Comedy", "Dante Alighieri", "2001"),
     };
 
     public ArrayList<BibliotecaBook> getAllBooks() {
