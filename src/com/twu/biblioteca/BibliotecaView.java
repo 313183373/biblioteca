4d5cfaf634297468ef9206a0c219a8475a7f9402
xion <313183373@qq.com>
2019-07-25 15:58:32 +0800

    1.4: implement view a menu of options

--- src/com/twu/biblioteca/BibliotecaView.java
+++ src/com/twu/biblioteca/BibliotecaView.java
@@ -4,6 +4,9 @@ import java.io.PrintStream;
 
 class BibliotecaView {
     public static final String WELCOME = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
+    public static final String[] menu = {"List of books"};
+
     private PrintStream out;
 
     BibliotecaView(PrintStream out) {
@@ -12,5 +15,12 @@ class BibliotecaView {
 
     void welcome() {
         out.println(WELCOME);
+    }
+
+    void showMenu() {
+        for (int i = 0; i < menu.length; i++) {
+            int index = i + 1;
+            out.println(index + ". " + menu[i]);
+        }
     }
 }
